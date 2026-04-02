77ed196bb64998d22fe8286701b5e3ce213ec510
Sumit-Kumar04 <sumitkumar4102004b@gmail.com>
2026-04-02 19:04:57 +0530

    Made some modification

--- backend/index.js
+++ backend/index.js
@@ -136,12 +136,14 @@ app.post("/api/auth/login", async (req, res) => {
 
     const token = generateToken(user._id);
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: false,
-      sameSite: "lax",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+   const isProduction = process.env.NODE_ENV === "production";
+
+res.cookie("token", token, {
+  httpOnly: true,
+  secure: isProduction,
+  sameSite: isProduction ? "none" : "lax",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
 
     res.status(200).json({
       success: true,
